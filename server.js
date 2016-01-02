32eb9b65d83e10d367717c19e080ae2b789e4ebc
Paul Sastrasinh <psastras@Pauls-MBP.fios-router.home>
2016-01-01 23:23:25 -0500

    Cache, Gzip, and Logging

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ var express = require('express');
 var path = require('path');
 var webpack = require('webpack');
 var app = express();
+var compression = require('compression');
+var morgan = require('morgan');
 
 var isDevelopment = (process.env.NODE_ENV !== 'production');
 var static_path = path.join(__dirname, 'public');
+var defaultPort = 8080;
 
 if (isDevelopment) {
   var config = require('./webpack.config');
@@ -14,17 +17,20 @@ if (isDevelopment) {
     publicPath: config.output.publicPath,
     hot: true,
     inline: true
-  }).listen(8080, 'localhost', function (err, result) {
+  }).listen(defaultPort, 'localhost', function (err, result) {
     if (err) { console.log(err) }
     console.log('Listening at localhost:8080');
   });
 } else {
-  app.use(express.static(static_path))
+  app.use(morgan('combined'));
+  app.use(compression());
+  app.use(express.static(static_path, { maxAge: 86400000 }))
     .get('/', function (req, res) {
       res.sendFile('index.html', {
         root: static_path
-    });
-  }).listen(process.env.PORT || 8080, function (err) {
+      });
+    })
+  .listen(process.env.PORT || defaultPort, function (err) {
     if (err) { console.log(err) };
     console.log('Listening at localhost:8080');
   });
